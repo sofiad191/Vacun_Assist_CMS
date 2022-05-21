ebadec2b21f226643416258705cb7efa3418294e
Sofia <sofiadascanio1@gmail.com>
2022-05-21 18:36:00 -0300

    Cambios en registro e iniciar sesion. Cambios en las plantillas

--- Vacun_Assist/gestion_usuarios/views.py
+++ Vacun_Assist/gestion_usuarios/views.py
@@ -1,10 +1,8 @@
 import re
 from django.shortcuts import render, redirect
-# from django.template import loader
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
+#from django.contrib import messages
 from gestion_usuarios.forms import FormularioAutenticacion, FormularioRegistro
 
 
@@ -13,30 +11,6 @@ from gestion_usuarios.forms import FormularioAutenticacion, FormularioRegistro
 
 def inicio(request):
     return render(request, "gestion_usuarios/inicio.html")
-
-
-"""class registro(View):
-    def get(self, request):
-        form=UserCreationForm()
-        return render(request, "autenticacion/registro.html", {"form": form})
-
-    def post(self, request):
-        form=UserCreationForm(request.POST)
-
-        if form.is_valid():
-            usuario=form.save()
-
-            login(request, usuario)
-
-            return redirect('inicio')
-
-        else:
-            #Por cada mensaje de error en el formulario.
-            for msg in form.error_messages:
-                messages.error(request, form.error_messages[msg])
-
-            #Devuelve el formulario como estaba antes (los campos validos) + los errores detallados  
-            return render(request, "autenticacion/registro.html", {"form": form})"""
     
 def registro(request):
     #Entra una vez que aprieta el boton de enviar
@@ -47,6 +21,7 @@ def registro(request):
             infForm=miFormulario.cleaned_data
 
             #Aca seria registrar la info
+            #Mandar mail con el codigo generado
 
     else:
         #Si entra al else, seria el formulario vacio, para que llene los datos
@@ -60,37 +35,6 @@ def cerrar_sesion(request):
     logout(request)
     return redirect('inicio')
 
-
-"""def iniciar_sesion(request):
-
-    #Si se apreto el boton:
-    if request.method=="POST":
-        return redirect('inicio')
-        form=AuthenticationForm(request, data=request.POST)
-    
-        if form.is_valid():
-            #La informacion que ingreso el usuario
-            nombre_usuario=form.cleaned_data.get("username")
-            contra=form.cleaned_data.get("password")
-
-            #Autenticar el usuario
-            usuario=authenticate(username=nombre_usuario, password=contra)
-
-            #Si existe el usuario
-            if usuario is not None:
-                login(request, usuario)
-                return redirect('inicio')
-
-            else:
-                messages.error(request, "usuario no valido")
-
-        else:
-            messages.error(request, "Informacion incorrecta")
-
-    #Mientras que no se apriete el boton:
-        
-    form=AuthenticationForm()
-    return render(request, "autenticacion/login.html", {"form": form})"""
 
 def iniciar_sesion(request):
     if request.method=="POST":
